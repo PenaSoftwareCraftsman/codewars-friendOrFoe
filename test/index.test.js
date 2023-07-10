ee0b253fed0aecd28d66c3cc49463f6e54145493
PenaSoftwareCraftsman <jose.henrique.pena.goulart@gmail.com>
2023-07-10 20:17:57 -0300

    test: Should return [] when the input is ["Kieran"]

--- test/index.test.js
+++ test/index.test.js
@@ -4,4 +4,8 @@ describe('friend', () => {
     it('Should return [] when the input is empty', () => {
         expect(friend([])).toStrictEqual([]);
     });
+
+    it('Should return [] when the input is ["Kieran"]', () => {
+        expect(friend(["Kieran"])).toStrictEqual([]);
+    });
 });
