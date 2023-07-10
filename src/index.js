ab6cdfd50ca89ce488f75a3c014fba830fcdd342
PenaSoftwareCraftsman <jose.henrique.pena.goulart@gmail.com>
2023-07-10 20:40:15 -0300

    test: Should return ["Ryan] when the input is ["Ryan"]

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
-const friend = () => {
+const friend = (input) => {
     let result = [];
+
+    for(str of input) {
+        if(str.length == 4){
+            result.push(str);
+        }
+    }
 
     return result;
 }
--- test/index.test.js
+++ test/index.test.js
@@ -8,4 +8,8 @@ describe('friend', () => {
     it('Should return [] when the input is ["Kieran"]', () => {
         expect(friend(["Kieran"])).toStrictEqual([]);
     });
+
+    it('Should return ["Ryan] when the input is ["Ryan"]', () => {
+        expect(friend(["Ryan"])).toStrictEqual(["Ryan"]);
+    });
 });
